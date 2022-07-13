e17a0b94ae765c4797cc0a2551622c67ab78d76f
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-07-13 18:55:22 +0600

    isort imports

--- src/core/settings.py
+++ src/core/settings.py
@@ -108,8 +108,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ),
